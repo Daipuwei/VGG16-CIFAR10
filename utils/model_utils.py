2281915f8301f2cab487a144becd6d9720b9e2ae
DaiPuWei <771830171@qq.com>
2020-03-25 00:19:59 +0800

    Add files via upload

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -5,16 +5,8 @@
 # @File    : model_utils.py
 # @Software: PyCharm
 
+import tensorflow as tf
 from tensorflow import keras as K
-
-def binary_cross_entropy_loss(y_true,y_pred):
-    """
-    这是定义二分类交叉熵损失的函数
-    :param y_true:真实标签
-    :param y_pred:预测标签
-    :return:
-    """
-    return K.losses.binary_crossentropy(y_true,y_pred)
 
 def categorical_crossentropy_loss(y_true,y_pred):
     """
@@ -23,16 +15,7 @@ def categorical_crossentropy_loss(y_true,y_pred):
     :param y_pred:预测标签
     :return:
     """
-    return K.losses.categorical_crossentropy(y_true,y_pred)
-
-def binary_accuracy(y_true,y_pred):
-    """
-    这是二分类的精度函数
-    :param y_true: 真实标签
-    :param y_pred: 预测标签
-    :return:
-    """
-    return K.metrics.binary_accuracy(y_true,y_pred)
+    return tf.nn.softmax_cross_entropy_with_logits_v2(y_true,y_pred)
 
 def categorical_accuracy(y_true,y_pred):
     """
@@ -41,7 +24,10 @@ def categorical_accuracy(y_true,y_pred):
     :param y_pred: 预测标签
     :return:
     """
-    return K.metrics.categorical_accuracy(y_true,y_pred)
+    correct_prediction = tf.equal(tf.argmax(y_true, 1), tf.argmax(y_pred, 1))
+    # Operation calculating the accuracy of our predictions
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return accuracy
 
 # 这是数据平均类
 class AverageMeter(object):
